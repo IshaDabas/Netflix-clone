64a1f14a6024b59f5b1a8a17da7fc396dfc8a972
IshaDabas <ishadabas@icloud.com>
2022-05-27 19:41:12 +0530

    Rows Pending

--- src/Banner.js
+++ src/Banner.js
@@ -39,17 +39,15 @@ function Banner() {
       }}
     >
       <div className="banner_contents">
-        <h1 className="banner_title">Movie Name </h1>
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name} 
+          </h1>
         <div className="banner_buttons">
           <button className="banner_button">PLAY</button>
           <button className="banner_button">My list</button>
         </div>
         <h1 className="banner_description">
-          {truncate(
-            `Welcome to Netflix! Here we stream movies and series which you all will love the most.
-            It wont be easy to get rid of us. Enjoy your time.`,
-            50
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--fadeBottom" />
