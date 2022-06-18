bf897c66cb398cc63c1741651126831157b2b532
IshaDabas <ishadabas@icloud.com>
2022-06-18 20:02:22 +0530

    Firebase added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./App.css";
 import Homescreen from "./Homescreen";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Rout
+} from "react-router-dom" 
 function App() {
   return (
     <div className="app">
       <Homescreen />
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -4,5 +4,5 @@ const firebaseConfig = {
   projectId: "netflix-clone-97894",
   storageBucket: "netflix-clone-97894.appspot.com",
   messagingSenderId: "441700793457",
-  appId: "1:441700793457:web:7d3c62f7e6b4aa4efc6fd0",
+  appId: "1:441700793457:web:7d3c62f7e6b4aa4efc6fd0"
 };
