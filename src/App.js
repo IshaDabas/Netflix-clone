28d5a2f70147ebe12253336a094c4d724f91fe94
IshaDabas <ishadabas@icloud.com>
2022-06-30 22:20:06 +0530

    screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import Homescreen from "./screens/Homescreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import loginScreen from "./loginScreen";
+
 function App() {
+  const user = null;
   return (
     <div className="app">
       <Router>
+        {!user ? (
+            <loginSCreen />
+          ): }
+
         <Routes>
           <Route path="/" element = {<Homescreen/>}>
           </Route>
@@ -15,3 +22,4 @@ function App() {
 }
 
 export default App;
+ 
---
+++ src/loginScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './loginScreen.css'
+function loginScreen() {
+  return (
+    <div className='loginscreen'>
+
+    </div>
+  )
+}
+
+export default loginScreen
